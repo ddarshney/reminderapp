97ddb06fba57d52e9e43ffbf3d3d1584118c202b
Aishani2001 <66082466+Aishani2001@users.noreply.github.com>
2021-01-06 16:38:29 +0530

    Update sign_in.dart

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -20,75 +20,112 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[100],
+    return new Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        title: Text('Sign in to Remind Me App'),
+        backgroundColor: Colors.green,
+        elevation: 10.0,
         actions: <Widget>[
           FlatButton.icon(
             icon: Icon(Icons.person),
-            label: Text('Register'),
+            label: Text('SIGN UP',
+            style:TextStyle(fontWeight: FontWeight.bold,
+
+            ),),
             onPressed: () => widget.toggleView(),
           ),
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 60.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 15.0,
-              ),
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+        Container(
+        child: Stack(
+          children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(15.0, 100.0, 0.0, 0.0),
+            child: Text('Hello',
+            style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+    ),
+          Container(
+          padding: EdgeInsets.fromLTRB(18.0, 175.0, 0.0, 0.0),
+            child: Text('There',
+            style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+    ),
+          Container(
+            padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+            child: Text('.',
+              style: TextStyle(fontSize: 80.0,fontWeight: FontWeight.bold,color: Colors.green)),
+          )
+        ],
+      ),
+    ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.person),
+                        labelText: 'E-MAIL',
+                        labelStyle:TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey),
+    focusedBorder: UnderlineInputBorder(
+    borderSide: BorderSide(color: Colors.green))),
+                    validator: (val) => val.isEmpty ? 'NO E-MAIL ID ENTERED' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
+                  ),
+              SizedBox(height: 30.0),
               TextFormField(
                 decoration: InputDecoration(
-                    icon: Icon(Icons.person),
-                    labelText: 'Enter your e-mail ID'),
-                validator: (val) => val.isEmpty ? 'No e-mail ID entered' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 15.0),
-              TextFormField(
-                decoration: InputDecoration(
-                    icon: Icon(Icons.lock), labelText: 'Enter your password'),
+                    icon: Icon(Icons.lock), labelText: 'PASSWORD',
+    labelStyle:TextStyle(
+    fontWeight: FontWeight.bold, color: Colors.grey),
+    focusedBorder: UnderlineInputBorder(
+    borderSide: BorderSide(color: Colors.green))),
+                obscureText: true,
                 validator: (val) => val.length < 6
-                    ? 'Enter a password with 6 or more characters'
+                    ? 'PASSWORD SHOULD HAVE 6 OR MORE CHARACTERS'
                     : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
               ),
-              SizedBox(height: 15.0),
+              SizedBox(height: 20.0),
               RaisedButton(
-                  color: Colors.blue[400],
+                  color: Colors.green,
                   child: Text(
-                    'Sign In',
+                    'SIGN IN',
                     style: TextStyle(color: Colors.white),
                   ),
                   onPressed: () async {
                     if (_formKey.currentState.validate()) {
                       dynamic result =
-                          await _auth.signinwithemailpassword(email, password);
+                      await _auth.signinwithemailpassword(email, password);
                       if (result == null) {
                         setState(() {
-                          error = 'Could not sign in with those credentials';
+                          error = 'ERROR SIGNING IN';
                         });
                       }
                     }
                   }),
-              SizedBox(height: 20.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 15.0),
-              ),
+
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 15.0),
+                  ),
             ],
           ),
         ),
+            ),
+            ],
       ),
     );
   }
